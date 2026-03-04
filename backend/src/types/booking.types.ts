ae50556a246aeaf7c78601e53f23fa5e4240a68c
Hamza Mustafa <hmustafe299@gmail.com>
2026-03-04 17:03:13 +0300

    started frontend

---
+++ backend/src/types/booking.types.ts
@@ -0,0 +1,7 @@
+export interface ICreateBooking {
+  carId: number;
+  userId: number;
+  startDate: Date;
+  endDate: Date;
+  totalPrice: number;
+}
---
+++ frontend/components/login.tsx
@@ -0,0 +1,9 @@
+const LoginForm = () => {
+  return (
+    <div className="min-h-screen min-w-full flex justify-center items-center bg-gray-700 text-white">
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default LoginForm;
